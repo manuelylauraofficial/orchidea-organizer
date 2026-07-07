a453512e4ce54e6cdc3683a9a30bdb90f6cb8a81
manuel <manuelledoremixes@gmail.com>
2026-07-07 10:18:34 +0200

    import react

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react'
+import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { supabase } from './lib/supabase'
 
 const TABS = [
